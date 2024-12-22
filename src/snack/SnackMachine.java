c2244f69334a7c2df8dee6b66db1f9d4acf2162a
Irvin Samboy <contacoirvinsamboy@gmail.com>
2024-12-22 19:30:29 -0400

    creating add product function

--- src/snack/SnackMachine.java
+++ src/snack/SnackMachine.java
@@ -1,5 +1,7 @@
 package snack;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SnackMachine {
@@ -9,6 +11,7 @@ public class SnackMachine {
 
     static void snackMachine() {
         Scanner read = new Scanner(System.in);
+        List<Snack> products = new ArrayList<>();
         int option = 0;
         boolean exit = false;
         System.out.println("--SNACK MACHINE--");
@@ -17,6 +20,7 @@ public class SnackMachine {
         while (!exit) {
             try{
                 option = showMenu(read);
+                exit = executeOptions(option, read, products);
             }
             catch (Exception e) {
                 System.out.println("An error was ocurred " + e.getMessage());
@@ -34,6 +38,37 @@ public class SnackMachine {
                 """);
 
         return Integer.parseInt(read.nextLine());
+    }
+
+    static boolean executeOptions (int option, Scanner read, List<Snack> products) {
+            boolean exit = false;
+
+            switch (option) {
+                case 1 -> addProduct(read, products);
+            }
+
+            return exit;
+    }
+
+    static void addProduct (Scanner read, List<Snack> products ) {
+        System.out.println("Select product to buy");
+
+        Snacks.showSnacks();
+
+        int id = Integer.parseInt(read.nextLine());
+            boolean productFound = false;
+            for(Snack snack : Snacks.getSnacks()) {
+                if(snack.getIdSnack() == id) {
+                    products.add(snack);
+                    System.out.println("Product added correctly");
+                    productFound = true;
+                    break;
+                }
+            }
+
+            if(!productFound) {
+                System.out.println("Product not found");
+            }
     }
 }
 
