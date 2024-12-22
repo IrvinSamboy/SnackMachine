c4d396f7d591b3312938a03c73c25cf843a9a44f
Irvin Samboy <contacoirvinsamboy@gmail.com>
2024-12-22 18:32:26 -0400

    Optimizing code

--- src/snack/SnackMachine.java
+++ src/snack/SnackMachine.java
@@ -4,28 +4,36 @@ import java.util.Scanner;
 
 public class SnackMachine {
     public static void main(String[] args) {
+        snackMachine();
+    }
 
+    static void snackMachine() {
         Scanner read = new Scanner(System.in);
-
-        int options = 0;
-
+        int option = 0;
+        boolean exit = false;
         System.out.println("--SNACK MACHINE--");
         Snacks.showSnacks();
 
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
+        while (!exit) {
+            try{
+                option = showMenu(read);
+            }
+            catch (Exception e) {
+                System.out.println("An error was ocurred " + e.getMessage());
+            }
 
-        System.out.println("Menu:");
-        System.out.println("1. Buy snack");
-        System.out.println("2. Show ticket");
-        System.out.println("3. Add new snack");
-        System.out.println("4. Exit");
+        }
+    }
 
-        options = read.nextInt();
+    static int showMenu(Scanner read) {
+        System.out.println("""
+                1. Buy Snack
+                2. Show ticket
+                3. Ad new snack
+                4, Exit
+                """);
 
-
+        return Integer.parseInt(read.nextLine());
     }
 }
 
