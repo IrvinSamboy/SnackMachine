9722a68251a258cac7216acab480f7ecfae5b5d2
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-01-06 23:16:28 -0400

    Creating file snack service

---
+++ src/aplication/FileSnackServices.java
@@ -0,0 +1,27 @@
+package aplication;
+
+import domain.Snack;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileSnackServices implements  ISnacksServices{
+
+    @Override
+    public void setSnack(Snack snack) {
+
+    }
+
+    @Override
+    public void showSnacks() {
+
+    }
+
+    @Override
+    public List<Snack> getSnacks() {
+        return List.of();
+    }
+}
