d629985d5dcd953f2001274da55d0fa85aa861cc
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-01-06 23:16:50 -0400

    Creating set snack funcionality

--- src/aplication/FileSnackServices.java
+++ src/aplication/FileSnackServices.java
@@ -10,9 +10,21 @@ import java.util.List;
 
 public class FileSnackServices implements  ISnacksServices{
 
+    private final String fileName = "snacks.txt";
+    private File file = new File(fileName);
+    List<Snack> snacks = new ArrayList<>();
+
     @Override
     public void setSnack(Snack snack) {
+        this.snacks.add(snack);
+        this.addSnacksToFile(snack);
+    }
 
+    private void addSnacksToFile(Snack snack) {
+        boolean append = false;
+        PrintWriter writer = new PrintWriter(new FileWriter(file));
+        writer.println(snack);
+        writer.close();
     }
 
     @Override
